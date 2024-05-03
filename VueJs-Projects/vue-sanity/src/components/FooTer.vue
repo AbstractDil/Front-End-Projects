f24cac4e285a37ea6e1b39159815df072b98b356
AbstractDil <nandysagar@yahoo.com>
2024-05-04 01:50:55 +0530

    vue js with sanity project

---
+++ VueJs-Projects/vue-sanity/src/components/FooTer.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Footer Starts Here -->
+  <div class="container">
+  <footer class=" py-3 my-2 border-top">
+    
+  <div class="text-center">
+    <p class=""> Developed Using<a href="https://getbootstrap.com/"> Bootstrap </a> &amp; <a href="https://vuejs.org">VueJS</a> , by <a href="https:/www/nandysagar.in/"> Beyiman Dil <i class="bi bi-heartbreak-fill text-danger"></i></a>.</p>
+    <p class="text-sm">
+       For more information contact at <a href="mailto:nandysagar@yahoo.com">nandysagar@yahoo.com</a>
+    </p>
+  </div>
+
+  </footer>
+</div>
+  <!-- Footer Ends Here -->
+
+</template>
+
+<script>
+    export default {
+
+        components: "FooTer",
+        
+    }
+</script>
+
