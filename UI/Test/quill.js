9479567de0c5ae1036558235a60d13f26bd4351b
AbstractDil <nandysagar@yahoo.com>
2024-06-30 23:41:41 +0530

    katex updated

---
+++ UI/Test/quill.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var quill = new Quill('#editor-container', {
+        theme: 'snow',
+        modules: {
+            toolbar: [
+                [{ 'header': [1, 2, false] }],
+                ['bold', 'italic', 'underline'],
+                ['image','formula']  // Custom button for MathJax
+            ]
+        }
+    });
+
+  
+});
