ace823ffb6f31e23fe109b410ae3991440d3e284
AbstractDil <nandysagar@yahoo.com>
2026-02-26 18:09:24 +0530

    updated

--- MathHub-Students/QPEngine/js/qpickerData.js
+++ MathHub-Students/QPEngine/js/qpickerData.js
@@ -1,9 +1,8 @@
 // Default sample data — edit or replace via the UI
 const defaultStudents = [
-  "Sandip",
-  "Sunanda",
-  "Annesha",
-  "Subhangini",
+ "Sun",
+ "Swarnadip",
+ "Adrij"
 ];
 
-const defaultQuestionSets = ["1PZ6", "Y78G", "OR5F", "A15F", "AMXF", "OEHP", "8T7M", "WUF8"];
+const defaultQuestionSets = ["Y6EQ","SVTB","J0FO","GR1U","CRMH","9LCG","3GKG"];
